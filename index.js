b4326f6a68c3b238dd35bdae2e26493322301245
jmarualgit <jairelmarual@gmail.com>
2024-03-06 20:23:07 -0800

    Add a reset button
    This button will reset the created grid back to the default colors.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ let gridSize = 0;
 
 document.getElementById("makeGridButton").addEventListener("click", () => {
     gridSize = prompt("How many boxes!");
+
+    if (gridSize <= 0) {
+        gridSize = prompt("Please enter an amount between 0 and 100!");
+    }
+
+    if (gridSize >= 100) {
+        gridSize = prompt("Please enter an amount between 0 and 100!");
+    }
+    
     console.log(`${gridSize}` + " boxes are called!");
 
     createGrid(gridSize);
@@ -27,7 +36,25 @@ function createGrid (gridSize) {
     }
 
     document.getElementById("makeGridButton").style.visibility = "hidden";
+    showResetButton();
 }
 
 document.getElementById("container").style.display = "flex";
 document.getElementById("container").style.flexWrap = "wrap";
+
+function showResetButton() {
+    const resetContainer = document.getElementById("footer");
+
+    const resetButton = document.createElement("button");
+    resetButton.innerText = "Reset!"
+
+    resetButton.addEventListener("click", () => {
+        const divList = document.querySelectorAll("#childDiv");
+
+        for (let i = 0; i < divList.length; i++) {
+            divList[i].style.backgroundColor = "lightgray";
+        }
+    });
+
+    resetContainer.appendChild(resetButton);
+}
