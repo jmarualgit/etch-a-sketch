aec7d3e585379f0cef237ef5364dc3727a42e0d6
jmarualgit <jairelmarual@gmail.com>
2024-03-06 16:28:27 -0800

    Make prompt button to make grid
    Made it possible to make any grid size by giving the user a prompt window.

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-for (var i = 0; i < (16 * 16); i++) {
+let gridSize = 0;
 
-    const div = document.createElement("div");
-    div.id = "childDiv";
-    div.style.width = "3em";
-    div.style.margin = "0";
-    div.style.padding = "0";
-    div.style.flex = "0 1 calc(6.25%)";
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "#777";
-    });
-    //div.innerHTML = "hi";
+document.getElementById("makeGridButton").addEventListener("click", () => {
+    gridSize = prompt("How many boxes!");
+    console.log(`${gridSize}` + " boxes are called!");
 
-    document.getElementById("container").appendChild(div);
+    createGrid(gridSize);
+});
+
+function createGrid (gridSize) {
+    let divisor = 100 / gridSize;
+
+    for (var i = 0; i < (gridSize * gridSize); i++) {
+
+        const div = document.createElement("div");
+        div.id = "childDiv";
+        div.style.width = "3em";
+        div.style.margin = "0";
+        div.style.padding = "0";
+        div.style.flex = `0 1 calc(${divisor}%)`;
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "#777";
+        });
+        //div.innerHTML = "hi";
+    
+        document.getElementById("container").appendChild(div);
+    }
+
+    document.getElementById("makeGridButton").style.visibility = "hidden";
 }
 
 document.getElementById("container").style.display = "flex";
