6d8c6337c15c0c46bf912bc9a4ede8590f1ee97a
jmarualgit <jairelmarual@gmail.com>
2024-03-06 21:52:44 -0800

    Add grid darkening feature
    The grids will get darker each time it gets hovered upon.

--- index.js
+++ index.js
@@ -23,16 +23,49 @@ function createGrid (gridSize) {
 
         const div = document.createElement("div");
         div.id = "childDiv";
+        div.style.backgroundColor = "rgb(230, 230, 230)";
         div.style.width = "3em";
         div.style.margin = "0";
         div.style.padding = "0";
         div.style.flex = `0 1 calc(${divisor}%)`;
         div.addEventListener("mouseover", () => {
-            let r = Math.floor(Math.random() * 255);
-            let g = Math.floor(Math.random() * 255);
-            let b = Math.floor(Math.random() * 255);
-            
-            div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            let r, g, b;
+
+            if (div.style.backgroundColor == "rgb(230, 230, 230)") {
+                console.log("if");
+                r = Math.floor(Math.random() * 255);
+                g = Math.floor(Math.random() * 255);
+                b = Math.floor(Math.random() * 255);
+
+                div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            } else {
+                console.log(`else`);
+                console.log(div.style.backgroundColor);
+                let stringLength = div.style.backgroundColor.length;
+                let text = div.style.backgroundColor;
+
+                // get r value
+                let nextCommaLocation = text.indexOf(',');
+                let newRValue = parseInt(text.substring(4, nextCommaLocation));
+
+                // get g value
+                text = text.substring(nextCommaLocation + 2, stringLength);
+                nextCommaLocation = text.indexOf(',');
+                let newGValue = parseInt(text.substring(0, nextCommaLocation));
+
+                // get b value
+                let spaceIndex = text.indexOf(" ");
+                text = text.substring(spaceIndex + 1, text.length - 1);
+
+                // subtract values
+                newRValue = newRValue - 20;
+                newGValue -= 20;
+                newBValue = parseInt(text) - 20;
+
+                // assign new colors
+                newColor = `rgb(${newRValue}, ${newGValue}, ${newBValue})`
+                div.style.backgroundColor = newColor;
+            }
         });
         //div.innerHTML = "hi";
     
@@ -56,7 +89,7 @@ function showResetButton() {
         const divList = document.querySelectorAll("#childDiv");
 
         for (let i = 0; i < divList.length; i++) {
-            divList[i].style.backgroundColor = "lightgray";
+            divList[i].style.backgroundColor = "rgb(230, 230 ,230)";
         }
     });
 
